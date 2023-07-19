b74b925edbe99c3b69fd0fea8236af6fc7076d29
Yazky <yazki.maulana@gmail.com>
2023-07-19 23:56:19 +0700

    add: portofolio luffy using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./*.html'],
+  theme: {
+    extend: {
+
+      spacing: {
+        "big":"38rem",
+        "reg":"34rem",
+      }
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily:{
+      nunito: ['Nunito', 'sans-serif']
+    } 
+  },
+  plugins: [],
+}
+
